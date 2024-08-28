d17ce2816baadf64ef6b476cc6af981c5d1c85bf
rizalzall <102241575+rizalzall@users.noreply.github.com>
2024-08-28 08:37:07 +0700

    Menambahkan atau memperbarui plugin via API

--- plugins/owner-getfile.js
+++ plugins/owner-getfile.js
@@ -1,25 +1,33 @@
-import cp from 'child_process'
-import { promisify } from 'util'
-let exec = promisify(cp.exec).bind(cp)
-let handler = async (m, { conn, isROwner, usedPrefix, command, text }) => {
-    if (!text) throw `Nama Filenya?`
-    m.reply('Executing...')
-    let o
-    try {
-        o = await exec('type ' + text)
-    } catch (e) {
-        o = e
-    } finally {
-        let { stdout, stderr } = o
-        if (stdout.trim()) m.reply(stdout)
-        if (stderr.trim()) m.reply(stderr)
+/**
+  * Made by MannR
+  * ini wm njirr jan didelete
+  * https://whatsapp.com/channel/0029VaGqCO6I1rcjc9hisJ3U
+**/
+import fs from "fs"
+
+
+  let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+    if (!text) throw `Apana nama filenya?`
+
+    const filePath = `./${text}`;
+
+    // Check if the file exists
+    if (!fs.existsSync(filePath)) {
+      return m.reply(`File ${text} tidak ditemukan di direktori ./`);
     }
-}
 
+    try {
+      const fileContent = fs.readFileSync(filePath).toString();
+      m.reply(fileContent);
+    } catch (error) {
+    console.error(error);
+    m.reply(`[❗] Error: ${error.message || error}`);
+    }
+  }
+  
 handler.help = ['getfile']
-handler.tags = ['owner']
-handler.command = /^(getfile|gf)$/i
+handler.tags = ['owner'];
+handler.command = /^(getfile)$/i;
+handler.owner = true
 
-handler.rowner = true
-
-export default handler
+export default handler;
