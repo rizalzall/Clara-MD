7e9698d9e9743c63b06f5589110b68abccdccb72
rizalzall <102241575+rizalzall@users.noreply.github.com>
2024-08-28 07:45:59 +0700

    Menambahkan atau memperbarui plugin via API

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,47 +1,42 @@
-import ytdl from "ytdl-core";
+// [ DOWNLOADER-YTAUDIO/MP3 ]
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) return m.reply(`*Example:* .${command} https://www.youtube.com/xxxxxxx`);
-	conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
-	let obj = await ytmp3(text);
-	let title = obj.meta.title;
-	conn.sendFile(m.chat, obj.buffer, '', title, m)
+import ytdl from "node-yt-dl"; 
+
+const handler = async (m, { conn, command, text, usedPrefix }) => {
+/*
+kyzryzz.t.me
+Created by 𝘒𝘺𝘻𝘙𝘺𝘻𝘻 𝘟𝘋
+https://whatsapp.com/channel/0029VaRI1OB2P59cTdJKZh3q
+TITENONO LEK KO HAPUS😂
+*/
+  if (!text) {
+    throw `[❗] Linknya?\nContoh: ${usedPrefix + command} https://youtube.com/watch?v=7D03EZWcFVo`;
+  }
+
+  conn.sendMessage(m.chat, { react: { text: '⏱️', key: m.key } });
+
+  try {
+    let ytmp4 = await ytdl.mp4(text);
+
+    if (!ytmp4 || !ytmp4.media) {
+      throw new Error("Tidak dapat mengunduh audio. Coba lagi nanti.");
+    }
+
+    await conn.sendMessage(m.chat, { 
+      video: { url: ytmp4.media }, 
+      mimetype: 'video/mp4' 
+    }, { quoted: m });
+
+    conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
+    
+  } catch (error) {
+    console.error(error);
+    m.reply(`[❗] Error: ${error.message || error}`);
+  }
 };
 
-handler.help = ['ytmp4']
-handler.tags = ['downloader']
-handler.command = ['ytmp4', 'ytv']
-export default handler 
+handler.help = ['ytmp4'].map(v => v + ' <query>');
+handler.tags = ['downloader'];
+handler.command = /^(ytv|ytmp4|ytvideo)$/i;
 
-async function ytmp3(url) {
-	try {
-		const { videoDetails } = await ytdl.getInfo(url, {
-			lang: "id",
-		});
-		const stream = ytdl(url, {
-			filter: "videoandaudio",
-		});
-		const chunks = [];
-		stream.on("data", (chunk) => {
-			chunks.push(chunk);
-		});
-		await new Promise((resolve, reject) => {
-			stream.on("end", resolve);
-			stream.on("error", reject);
-		});
-		const buffer = Buffer.concat(chunks);
-		return {
-			meta: {
-				title: videoDetails.title,
-				channel: videoDetails.author.name,
-				seconds: videoDetails.lengthSeconds,
-				description: videoDetails.description,
-				image: videoDetails.thumbnails.slice(-1)[0].url,
-			},
-			buffer: buffer,
-			size: buffer.length,
-		};
-	} catch (error) {
-		throw error;
-	}
-}
+export default handler;
